ead9f463fcbbe01256ff6726be98441f697b93b1
KEST <jonathan.kest@cgi.com>
2022-06-28 19:27:05 +0200

    Create project for import data in h2

---
+++ src/main/java/cgi/hacker/vaillant/rien/d/impossible/Hackatton/HackattonApplication.java
@@ -0,0 +1,13 @@
+package cgi.hacker.vaillant.rien.d.impossible.Hackatton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackattonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackattonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cgi/hacker/vaillant/rien/d/impossible/Hackatton/HackattonApplicationTests.java
@@ -0,0 +1,13 @@
+package cgi.hacker.vaillant.rien.d.impossible.Hackatton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackattonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
