43c59cfbc4e94982892c2004acbfb856164a42a6
marciodiogo.antunesdearaujo <marciodiogo.antunesdearaujo@cgi.com>
2022-06-29 11:42:47 +0200

    Some corrections + improvements

--- src/main/java/cgi/hacker/vaillant/rien/d/impossible/Hackatton/HackattonApplication.java
+++ src/main/java/cgi/hacker/vaillant/rien/d/impossible/Hackatton/HackattonApplication.java
@@ -1,68 +1,17 @@
 package cgi.hacker.vaillant.rien.d.impossible.Hackatton;
 
 import cgi.hacker.vaillant.rien.d.impossible.Hackatton.service.CSVCreationService;
-import cgi.hacker.vaillant.rien.d.impossible.Hackatton.dto.MailDto;
+import cgi.hacker.vaillant.rien.d.impossible.Hackatton.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.mail.internet.MimeMessage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class HackattonApplication {
 
-    //TODO: IMPROVE, will not work on a war
-    private static final String RESOURCE_FOLDER = "src/main/resources/emls";
-    private static final CSVCreationService service = new CSVCreationService();
+    private static final MainService mainService = new MainService(new CSVCreationService());
 
-    public static void main(String[] args) throws FileNotFoundException {
-        createCSV(new File(args.length != 0 ? args[0]: RESOURCE_FOLDER));
-    }
-
-    public static void createCSV(File path) throws FileNotFoundException {
-        File[] files = path.listFiles();
-        AtomicInteger counter = new AtomicInteger(1);
-
-        if (files != null && files.length > 0) {
-            service.createCSVFromData(stream(files)
-                    .map(file -> display(file.getAbsoluteFile(), counter.getAndIncrement()))
-                    .collect(toList()));
-        } else {
-            System.out.println("\n===================");
-            System.out.println("No mails to be parsed");
-            System.out.println("=====================");
-        }
-    }
-
-    public static MailDto display(File emlFile, int number) {
-        try {
-            System.out.printf("File - %s =====================\n", number);
-
-            MimeMessage message = new MimeMessage(null, new FileInputStream(emlFile));
-
-            String from = message.getHeader("From", ",");
-            String to = message.getHeader("To", ",");
-            String copy = message.getHeader("CC", ",");
-
-            return MailDto.builder()
-                    .id(number)
-                    .from(from != null && from.contains(",") ? from.replaceAll(",", "|") : from)
-                    .to(to != null && to.contains(",") ? to.replaceAll(",", "|") : to)
-                    .copy(copy != null && copy.contains(",") ? copy.replaceAll(",", "|") : copy)
-                    .subject(message.getSubject())
-                    .date(message.getHeader("Date", ","))
-                    .build();
-
-        } catch (Exception e) {
-            // Error
-            return null;
-        }
+    public static void main(String[] args) {
+        mainService.generatedCSVFile(args);
     }
 }
--- src/main/java/cgi/hacker/vaillant/rien/d/impossible/Hackatton/service/MainService.java
+++ src/main/java/cgi/hacker/vaillant/rien/d/impossible/Hackatton/service/MainService.java
@@ -1,4 +1,93 @@
 package cgi.hacker.vaillant.rien.d.impossible.Hackatton.service;
 
+import cgi.hacker.vaillant.rien.d.impossible.Hackatton.dto.MailDto;
+import lombok.RequiredArgsConstructor;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.stream.Collectors.toList;
+
+@RequiredArgsConstructor
 public class MainService {
+
+    private final CSVCreationService service;
+
+    //TODO: IMPROVE, will not work on a war
+    private static final String RESOURCE_FOLDER = "src/main/resources/emls";
+
+    public void generatedCSVFile(String[] args) {
+        try {
+            createCSV(String.valueOf(new File(args.length != 0 ? args[0] : RESOURCE_FOLDER)));
+        } catch (FileNotFoundException e) {
+            System.out.println("\n============");
+            System.out.println("File not found");
+            System.out.println("==============");
+        }
+    }
+
+    private void createCSV(String directory) throws FileNotFoundException {
+        List<File> fileList = listFiles(directory);
+
+        AtomicInteger counter = new AtomicInteger(1);
+
+        if (fileList.size() > 0) {
+            service.createCSVFromData(fileList.stream()
+                    .map(file -> display(file.getAbsoluteFile(), counter.getAndIncrement()))
+                    .collect(toList()));
+        } else {
+            System.out.println("");
+            System.out.println("=====================");
+            System.out.println("No mails to be parsed");
+            System.out.println("=====================");
+        }
+    }
+
+    private static List<File> listFiles(final String directory) {
+        if (directory == null) {
+            return Collections.EMPTY_LIST;
+        }
+        List<File> fileList = new ArrayList<>();
+        File[] files = new File(directory).listFiles();
+        for (File element : files) {
+            if (element.isDirectory()) {
+                fileList.addAll(listFiles(element.getPath()));
+            } else {
+                fileList.add(element);
+            }
+        }
+        return fileList;
+    }
+
+    public MailDto display(File emlFile, int number) {
+        try {
+            System.out.printf("File - %s =====================\n", number);
+
+            MimeMessage message = new MimeMessage(null, new FileInputStream(emlFile));
+
+            String from = message.getHeader("From", ",");
+            String to = message.getHeader("To", ",");
+            String copy = message.getHeader("CC", ",");
+            String subject = message.getSubject();
+
+            return MailDto.builder()
+                    .id(number)
+                    .from(from != null && from.contains(",") ? from.replaceAll(",", "|") : from)
+                    .to(to != null && to.contains(",") ? to.replaceAll(",", "|") : to)
+                    .copy(copy != null && copy.contains(",") ? copy.replaceAll(",", "|") : copy)
+                    .subject(subject != null && subject.contains(",") ? subject.replaceAll(",", "") : subject)
+                    .date(message.getHeader("Date", ","))
+                    .build();
+
+        } catch (Exception e) {
+            // Error
+            return null;
+        }
+    }
 }
